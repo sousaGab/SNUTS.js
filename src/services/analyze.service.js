3d783b72ca415778f9ee5d513daa5208a5d4f472
jhonatanmizu <natanjesuss20@gmail.com>
2024-05-13 23:58:18 -0300

    :construction: building analyze service logic

--- src/services/analyze.service.js
+++ src/services/analyze.service.js
@@ -1,38 +1,25 @@
 import helpers from "../common/helpers/index.js";
 import path from "node:path";
-import {
-  detectAnonymousTest,
-  detectControlLogic,
-  detectSensitiveEquality,
-} from "../common/detectors/index.js";
+import { detectors } from "../common/detectors/index.js";
+import astService from "./ast.service.js";
+import { stringify } from "flatted";
 class AnalyzeService {
   async handleAnalyze(repoUrl) {
     try {
       const __dirname = path.dirname("");
       const directory = path.resolve(__dirname, "./public");
-      // const isDeleted = await helpers.deleteDownloadRepositories(directory);
-      // if (!isDeleted) return;
+      // await helpers.deleteDownloadRepositories(directory);
       // await helpers.downloadRepository(repoUrl, directory);
       const testFiles = await helpers.findTestFiles(directory);
-      const astFiles = testFiles.map((tf) => helpers.parseFile(tf));
-      console.log("🚀 ~ AnalyzeService ~ handleAnalyze ~ astFiles:", astFiles);
-      //   const result = [];
-      //   const resultAnonymousTest = detectAnonymousTest(ast[0]);
-      //   console.log(
-      //     "🚀 ~ AnalyzeService ~ handleAnalyze ~ resultAnonymousTest:",
-      //     resultAnonymousTest
-      //   );
-      //   const resultControlLogic = detectControlLogic(ast[0]);
-      //   console.log(
-      //     "🚀 ~ AnalyzeService ~ handleAnalyze ~ resultControlLogic:",
-      //     resultControlLogic
-      //   );
-      //   const resultSensitiveEquality = detectSensitiveEquality(ast[0]);
-      //   console.log(
-      //     "🚀 ~ AnalyzeService ~ handleAnalyze ~ resultSensitiveEquality:",
-      //     resultSensitiveEquality
-      //   );
-      //   return result;
+      const astFiles = testFiles.map((tf) => astService.parseToAst(tf));
+      const result = [];
+      astFiles.forEach((ast) => {
+        detectors.forEach((detector) => {
+          const smells = detector(ast);
+          result.push({ smell: detector.name, smells });
+        });
+      });
+      return result;
     } catch (error) {
       console.error("Error when we tried to handle analyze", error);
     }
