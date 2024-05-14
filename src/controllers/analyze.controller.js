1d7fad6ca2af9ad5ce153bc2044baaa7f03826e1
jhonatanmizu <natanjesuss20@gmail.com>
2024-05-13 23:54:50 -0300

    :sparkles: feat: add data result in analyze controller

--- src/controllers/analyze.controller.js
+++ src/controllers/analyze.controller.js
@@ -1,5 +1,4 @@
 import analyzeService from "../services/analyze.service.js";
-
 class AnalyzeController {
   async fetch(request, reply) {
     reply.send({ hello: "world" });
@@ -8,7 +7,7 @@ class AnalyzeController {
   async store(request, reply) {
     const { repository } = request.body;
     const result = await analyzeService.handleAnalyze(repository);
-    reply.send({ message: "stored", result });
+    reply.send({ data: result });
   }
 }
 
