cf14b6ad20a177dab686aec5445544efb41a909d
jhonatanmizu <natanjesuss20@gmail.com>
2024-05-20 00:17:36 -0300

    :sparkles: feat: commented csv.helper

--- src/common/helpers/csv.helper.js
+++ src/common/helpers/csv.helper.js
@@ -1,29 +1,29 @@
-import astService from "../../services/ast.service";
+// import astService from "../../services/ast.service";
 
-const createCSV = (files = []) => {
-  const headers = [
-    "file",
-    "describeCount",
-    "itCount",
-    "complexSnapshots",
-    "identicalTestDescription",
-    "nonFunctionalStatement",
-    "onlyTest",
-    "subOptimalAssert",
-    "unusedImports",
-    "verboseTest",
-    "verifyInSetup",
-  ];
-  const csv = [headers];
-  csv[0] = headers.join(",");
-  files.forEach((file) => {
-    const row = [file];
+// const createCSV = (files = []) => {
+//   const headers = [
+//     "file",
+//     "describeCount",
+//     "itCount",
+//     "complexSnapshots",
+//     "identicalTestDescription",
+//     "nonFunctionalStatement",
+//     "onlyTest",
+//     "subOptimalAssert",
+//     "unusedImports",
+//     "verboseTest",
+//     "verifyInSetup",
+//   ];
+//   const csv = [headers];
+//   csv[0] = headers.join(",");
+//   files.forEach((file) => {
+//     const row = [file];
 
-    try {
-      const ast = astService.parseToAst(file);
-      const infos = astService.getTestInfo();
-      row.push(infos.describeCount);
-      row.push(infos.itCount);
-    } catch (error) {}
-  });
-};
+//     try {
+//       const ast = astService.parseToAst(file);
+//       const infos = astService.getTestInfo();
+//       row.push(infos.describeCount);
+//       row.push(infos.itCount);
+//     } catch (error) {}
+//   });
+// };
