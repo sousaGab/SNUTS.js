51af0722a64bfa133aac0cada50e658e57ca0c55
jhonatanmizu <natanjesuss20@gmail.com>
2024-05-25 22:37:40 -0300

    :sparkles: feat: implement handle analyze csv

--- src/services/analyze.service.js
+++ src/services/analyze.service.js
@@ -8,7 +8,6 @@ class AnalyzeService {
     try {
       const __dirname = path.dirname("");
       const directory = path.resolve(__dirname, "./public");
-      await helpers.deleteDownloadRepositories(directory);
       const repoFolder = helpers.getRepositoryFolder(repoUrl);
       await helpers.downloadRepository(repoUrl, repoFolder);
       const testFiles = await helpers.findTestFiles(directory);
@@ -24,9 +23,37 @@ class AnalyzeService {
           };
         });
       });
+      await helpers.deleteDownloadRepositories(directory);
       return astFiles.flat();
     } catch (error) {
       console.error("Error when we tried to handle analyze", error);
+      return [];
+    }
+  }
+  async handleAnalyzeToCSV(repoUrl) {
+    try {
+      const __dirname = path.dirname("");
+      const directory = path.resolve(__dirname, "./public");
+      const repoFolder = helpers.getRepositoryFolder(repoUrl);
+      await helpers.downloadRepository(repoUrl, repoFolder);
+      const testFiles = await helpers.findTestFiles(directory);
+      const astFiles = testFiles.map((tf) => {
+        const testAst = astService.parseToAst(tf);
+        const testInfo = astService.getTestInfo(testAst);
+        return detectors.map((detector) => {
+          return {
+            file: helpers.getPathAfterPublic(tf),
+            type: detector.name.replace("detect", ""),
+            smells: detector(testAst),
+            itCount: testInfo.itCount,
+            describeCount: testInfo.describeCount,
+          };
+        });
+      });
+      await helpers.deleteDownloadRepositories(directory);
+      return astFiles.flat();
+    } catch (error) {
+      console.error("Error when we tried to handle analyze to csv", error);
       return [];
     }
   }
