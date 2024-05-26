18605324e4377a8e936ada2943951949d0c217c5
jhonatanmizu <natanjesuss20@gmail.com>
2024-05-25 22:34:49 -0300

    :sparkles: feat: add start declaration and end declaration line

--- src/common/detectors/generalFixture.js
+++ src/common/detectors/generalFixture.js
@@ -1,47 +1,55 @@
 import traverse from "@babel/traverse";
 import * as t from "@babel/types";
 import astService from "../../services/ast.service.js";
-
 const traverseDefault = traverse.default;
 
 const detectGeneralFixture = (ast) => {
-  const setupVariables = new Set();
+  const setupVariables = new Map(); // Using a Map for easier lookup
   const usedVariables = new Set();
   const generalFixtureSmells = [];
+
+  const detectSetupVariables = (setupBody) => {
+    if (t.isBlockStatement(setupBody)) {
+      setupBody.body.forEach((statement) => {
+        if (t.isVariableDeclaration(statement)) {
+          statement.declarations.forEach((declaration) => {
+            if (t.isIdentifier(declaration.id)) {
+              setupVariables.set(declaration.id.name, {
+                startLine: declaration.loc.start.line,
+                endLine: declaration.loc.end.line,
+              });
+            }
+          });
+        }
+      });
+    }
+  };
 
   traverseDefault(ast, {
     CallExpression(path) {
       const { callee, arguments: args } = path.node;
 
-      // Detect variables set up in beforeAll or beforeEach
       if (
         t.isIdentifier(callee, { name: "beforeAll" }) ||
         t.isIdentifier(callee, { name: "beforeEach" })
       ) {
         if (args.length >= 1 && astService.isFunction(args[0])) {
-          const setupBody = args[0].body;
-          if (t.isBlockStatement(setupBody)) {
-            setupBody.body.forEach((statement) => {
-              if (t.isVariableDeclaration(statement)) {
-                statement.declarations.forEach((declaration) => {
-                  if (t.isIdentifier(declaration.id)) {
-                    setupVariables.add(declaration.id.name);
-                  }
-                });
-              }
-            });
-          }
+          detectSetupVariables(args[0].body);
         }
-      }
-      // Detect usage of setup variables in test cases
-      else if (/^(it|test)$/.test(callee.name) && args.length >= 2) {
+      } else if (/^(it|test)$/.test(callee.name) && args.length >= 2) {
         const testBody = args[1].body;
         if (t.isBlockStatement(testBody)) {
           traverseDefault(testBody, {
-            noScope: true, // Prevents creation of new scope
+            noScope: true,
             Identifier(innerPath) {
               if (setupVariables.has(innerPath.node.name)) {
-                usedVariables.add(innerPath.node.name);
+                usedVariables.add({
+                  name: innerPath.node.name,
+                  loc: {
+                    startLine: innerPath.node.loc.start.line,
+                    endLine: innerPath.node.loc.end.line,
+                  },
+                });
               }
             },
           });
@@ -50,10 +58,14 @@ const detectGeneralFixture = (ast) => {
     },
   });
 
-  // Identify general fixture smells
-  setupVariables.forEach((variable) => {
+  setupVariables.forEach((_, variable) => {
     if (!usedVariables.has(variable)) {
-      generalFixtureSmells.push(variable);
+      const { startLine, endLine } = setupVariables.get(variable);
+      generalFixtureSmells.push({
+        // ...variable,
+        startLine,
+        endLine,
+      });
     }
   });
 
